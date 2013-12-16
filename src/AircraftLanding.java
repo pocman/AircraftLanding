462c0751667d17459c45905bb86ecd9c0e9f1fde
THOMAS POCREAU <thomas.poc@gmail.com>
2013-12-16 09:21:06 +0100

    eclipse stuff

--- src/AircraftLanding.java
+++ src/AircraftLanding.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 import solver.Solver;
 import solver.variables.IntVar;
 import solver.variables.Task;
@@ -11,27 +14,50 @@ public class AircraftLanding {
 	 */
 	
 	Task[] activityPlanes;
+	int[] setOfTypes = new int[]{1,2,3};
+	Random r = new Random(42);
 	IntVar[] duration, landing, takeOff, track; //for each plane
-	int[] windowStart, windowDuration; //for each plane
+	int[] windowStart, windowDuration, windowEnd; //for each plane
 	int[] typePlane; //1,2 ou 3 correspondant à la capacité utilisée
 	int[] capacity; //of the track
 	int nPlanes; //number of planes
 	int nTracks; //number of tracks
+	
+	public AircraftLanding(String[] schedule, int nTracks){
+		
+		ArrayList<int[]> planes = new ArrayList<int[]>();
+		
+		for(String s : schedule){
+			String[] temp = s.split(":");
+			int[] planesByTF = new int[]{Integer.parseInt(temp[0]), Integer.parseInt(temp[1]), Integer.parseInt(temp[2])};
+			for(int i = 0; i < planesByTF[2]; i++){
+				planes.add(new int[]{planesByTF[0]*60, planesByTF[1]*60, });
+			}
+		}
+		
+		this.setnTracks(nTracks);
+		this.setnPlanes(wStart.length);
+		this.set
+	}
 	
 	public void model(Solver s){
 		
 		activityPlanes = new Task[nPlanes];
 		
 		for(int i = 0; i < nPlanes; i++){
+			landing[i] = VariableFactory.bounded("landing " + i, windowStart[i], windowEnd[i], s);
+			duration[i] = VariableFactory.bounded("duration on airport " + i , 30, windowDuration[i], s);
+			takeOff[i] = VariableFactory.bounded("landing " + i, windowStart[i], windowEnd[i], s);
+			
 			activityPlanes[i] = VariableFactory.task(landing[i], duration[i], takeOff[i]);
 		}
 		
 		//for each plane which track, it's on
 		track = VariableFactory.enumeratedArray("track of plane", nPlanes, 0,  nTracks, s);
 		
+		
 	}
-	
-
+		
 	public int[] getWindowStart() {
 		return windowStart;
 	}
---
+++ src/instanceGenerator.java
@@ -0,0 +1,34 @@
+
+public class instanceGenerator {
+
+	/**
+	 * @param args
+	 */
+	public static AircraftLanding generator(){
+		AircraftLanding al = new AircraftLanding();
+		
+		al.setnTracks(5);
+		al.setCapacity(new int[]{6,5,3,2,1});
+		
+		String[] schedule = new String[]{
+		"11:12:3",
+	    "12:13:2",
+	    "13:14:4",
+	    "14:15:4",
+	    "15:16:3",
+	    "16:17:2",
+	    "17:18:6",
+	    "18:19:4",
+	    "19:20:4",
+	    "20:21:4",
+	    "21:22:1",
+	    "22:23:1"};
+		
+		for(String s : schedule){
+			
+		}
+		
+		
+	}
+
+}
